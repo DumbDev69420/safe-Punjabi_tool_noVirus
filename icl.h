ce795834d103da510b49386ace6efce747ca0c3f
DumbDev69420 <116299301+DumbDev69420@users.noreply.github.com>
2022-10-20 18:49:39 +0200

    Add files via upload
    Didnt make it in the main because idk

---
+++ icl.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Windows.h>
+#include <vector>
+#include <TlHelp32.h>
+#include <Psapi.h>
---
+++ proc.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "icl.h"
+
+DWORD GetProcId(const wchar_t* procName);
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
